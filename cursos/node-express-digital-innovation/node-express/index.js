26307ced2274da75ed3a3c722588af848c6d487f
inessouza <inessouzasp@gmail.com>
2021-07-15 18:35:33 -0300

    Primeiro Módulo

---
+++ cursos/node-express-digital-innovation/node-express/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const userRoute = require('./routes/userRoute');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+const port = 3000;
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Hello World by Express!'));
+app.listen(port, () => console.log('Api rodando na porta 3000!'));
