eacb6b4350bd86c779feb3080bb6fa9731d3cb35
inessouza <inessouzasp@gmail.com>
2021-07-25 12:26:17 -0300

    Primeiro Módulo - Formação NodeJS

---
+++ cursos/formacao-node/primeiro-modulo/app.js
@@ -0,0 +1,9 @@
+var mostrarSite = true;
+const site = "www.nodejs.org"
+
+console.log("Hello World")
+console.log("Iniciando estudos em Node.JS")
+
+if (mostrarSite) {
+    console.log(site)
+}
---
+++ cursos/formacao-node/readme.md
@@ -0,0 +1,7 @@
+Por que usar Node.JS?
+
+- Muito leve
+- Muito rápido 
+- Usa JavaScript
+- Tem um dos maiores ecossistemas do mundo
+- Está sendo fortemente utilizado no mercado
