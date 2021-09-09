8f2f612f08c58dbe3bb5fc7d8c28fb250f778bfc
inessouza <inessouzasp@gmail.com>
2021-09-08 22:47:22 -0300

    Quinto Módulo

--- cursos/formacao-node/quinto-modulo/guia-perguntas/index.js
+++ cursos/formacao-node/quinto-modulo/guia-perguntas/index.js
@@ -1,16 +1,47 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const Pergunta = require("./database/Pergunta")
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão com o banco de dados realizada com sucesso!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro)
+    })
 
 // Informando ao Express o uso do EJS como View Engine
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+//app.use(express.text());
+
 app.get("/", (req, res) => {
-    res.render("index")
+    Pergunta.findAll({ raw: true }).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        })
+    })
 })
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/")
+    })
 })
 
 app.listen(8080, () => {
