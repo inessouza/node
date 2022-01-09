8b804ecea801f5d014693b61159a07444b169329
inessouza <inessouzasp@gmail.com>
2022-01-08 21:46:39 -0300

    Décimo Primeiro Módulo

--- cursos/formacao-node/modulo11/apidegames/index.js
+++ cursos/formacao-node/modulo11/apidegames/index.js
@@ -31,6 +31,85 @@ var db = {
 	]
 }
 
+app.get("/games", (req, res) => {
+	res.statusCode = 200
+	res.json(db.games)
+})
+
+app.get("/game/:id", (req, res) => {
+	if (isNaN(req.params.id)) {
+		res.sendStatus(400)
+	} else {
+		var id = parseInt(req.params.id)
+		var game = db.games.find(g => g.id == id)
+
+		if (game != undefined) {
+			res.statusCode = 200
+			res.json(game)
+		} else {
+			res.sendStatus(404)
+		}
+	}
+})
+
+app.post("/game", (req, res) => {
+	var { title, price, year } = req.body
+
+	db.games.push({
+		id: 2323,
+		title,
+		price,
+		year
+	})
+
+	res.sendStatus(200)
+})
+
+app.delete("/game/:id", (req, res) => {
+	if (isNaN(req.params.id)) {
+		res.sendStatus(400)
+	} else {
+		var id = parseInt(req.params.id)
+		var index = db.games.findIndex(g => g.id == id)
+
+		if (index == -1) {
+			res.sendStatus(404)
+		} else {
+			db.games.splice(index, 1)
+			res.sendStatus(200)
+		}
+	}	
+})
+
+app.put("/game/:id", (req, res) => {
+	if (isNaN(req.params.id)) {
+		res.sendStatus(400)
+	} else {
+		var id = parseInt(req.params.id)
+		var game = db.games.find(g => g.id == id)
+
+		if (game != undefined) {
+			var { title, price, year } = req.body
+
+			if (title != undefined) {
+				game.title = title
+			}
+
+			if (price != undefined) {
+				game.price = price
+			}
+
+			if (year != undefined) {
+				game.year = year
+			}
+
+			res.sendStatus(200)
+		} else {
+			res.sendStatus(404)
+		}
+	}	
+})
+
 app.listen(3000, () => {
 	console.log("Api Online")
 })
