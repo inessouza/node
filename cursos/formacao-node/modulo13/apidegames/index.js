0ac9d09cd6b0a59a977f6583ed96a72c33edf116
Ines Souza <inessouzasp@gmail.com>
2022-05-13 12:17:16 -0300

    Décimo Terceiro Módulo

--- cursos/formacao-node/modulo13/apidegames/index.js
+++ cursos/formacao-node/modulo13/apidegames/index.js
@@ -2,11 +2,36 @@ const express = require("express")
 const app = express();
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const jwt = require("jsonwebtoken")
+
+const JWTSEcret = "klj0930js0923"
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+function auth(req, res, next) {
+	const authToken = req.headers['authorization']
+	if (authToken != undefined) {
+		const bearer = authToken.split(' ')
+		var token = bearer[1]
+		jwt.verify(token, JWTSEcret, (err, data) => {
+			if (err) {
+				res.status(401)
+				res.json({ err: "Token Inválido"})
+			} else {
+				req.token = token
+				req.loggedUser = { id: data.id, email: data.email }
+				req.empresa = "Google"
+				next()
+			}
+		})
+	} else {
+		res.status(401)
+		res.json({ err: "Token Inválido!"})
+	}
+}
 
 var db = {
 	games: [
@@ -45,12 +70,12 @@ var db = {
 	]
 }
 
-app.get("/games", (req, res) => {
+app.get("/games", auth, (req, res) => {
 	res.statusCode = 200
-	res.json(db.games)
+	res.json({ empresa: req.empresa, user: req.loggedUser, games: db.games})
 })
 
-app.get("/game/:id", (req, res) => {
+app.get("/game/:id", auth, (req, res) => {
 	if (isNaN(req.params.id)) {
 		res.sendStatus(400)
 	} else {
@@ -66,7 +91,7 @@ app.get("/game/:id", (req, res) => {
 	}
 })
 
-app.post("/game", (req, res) => {
+app.post("/game", auth, (req, res) => {
 	var { title, price, year } = req.body
 
 	db.games.push({
@@ -79,7 +104,7 @@ app.post("/game", (req, res) => {
 	res.sendStatus(200)
 })
 
-app.delete("/game/:id", (req, res) => {
+app.delete("/game/:id", auth,(req, res) => {
 	if (isNaN(req.params.id)) {
 		res.sendStatus(400)
 	} else {
@@ -95,7 +120,7 @@ app.delete("/game/:id", (req, res) => {
 	}	
 })
 
-app.put("/game/:id", (req, res) => {
+app.put("/game/:id", auth, (req, res) => {
 	if (isNaN(req.params.id)) {
 		res.sendStatus(400)
 	} else {
@@ -124,16 +149,23 @@ app.put("/game/:id", (req, res) => {
 	}	
 })
 
-app.post("/auth", (req, res) => {
+app.post("/auth", auth,(req, res) => {
 	var { email, password } = req.body;
 
 	if (email != undefined) {
-		db.users.find(u => u.email == email)
+		var user = db.users.find(u => u.email == email)
 
 		if (user != undefined) {
 			if (user.password == password) {
-				res.status(200)
-				res.json({ token: "Token Falso" })
+				jwt.sign({ id: user.id, email: user.email }, JWTSEcret, {expiresIn: '48h'}, (err, token) => {
+					if(err) {
+						res.status(400)
+						res.json(({err: "Falha interna"}))
+					} else {
+						res.status(200)
+						res.json({ token: token })
+					}
+				})
 			} else {
 				res.status(401)
 				res.json({ err: "Credenciais Inválidas!" })
