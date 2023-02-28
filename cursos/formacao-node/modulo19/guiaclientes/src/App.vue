bb1b4edc14fa36bf1b9ed0a454835db8e695b61e
Ines Souza <inessouzasp@gmail.com>
2023-02-27 21:56:26 -0300

    VueJS - Components

--- cursos/formacao-node/modulo19/guiaclientes/src/App.vue
+++ cursos/formacao-node/modulo19/guiaclientes/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Módulo Vue JS</h1>
+    <ClienteVue/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ClienteVue from './components/ClienteVue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ClienteVue
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ cursos/formacao-node/modulo19/guiaclientes/src/components/ClienteVue.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="cliente"><h2>Cliente aqui!</h2></div>
+</template>
+
+<script>
+
+export default {
+    name: 'ClienteVue',
+}
+</script>
+
+<style scooped>
+    #cliente{
+        color: blue;
+        background-color: #D3D3D3;
+    }
+</style>
